b6491a6c70cc9a8c27149511db3406e8b56b47e1
Melkey <amokstakov@google.com>
2023-07-10 12:13:02 -0700

    Initial commit

---
+++ test/rag_stack_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RagStackCdk from '../lib/rag_stack_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rag_stack_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RagStackCdk.RagStackCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
