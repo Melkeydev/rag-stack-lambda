1770bc57a6a9113c698e0af8f1eb82a209185519
Amokstakov <amokstakov@gmail.com>
2023-07-17 18:15:18 -0700

    Adding more info for go file

--- lib/rag_stack_cdk-stack.ts
+++ lib/rag_stack_cdk-stack.ts
@@ -2,18 +2,34 @@ import * as cdk from "aws-cdk-lib";
 import { Stack, StackProps } from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 export class RagStackCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Define the DynamoDB table
+    // TODO: rename to user table
+    const table = new dynamodb.Table(this, "MyTable", {
+      partitionKey: { name: "username", type: dynamodb.AttributeType.STRING },
+      tableName: "user-table-name",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     // Define the Lambda function
     const myFunction = new lambda.Function(this, "MyFunction", {
       code: lambda.Code.fromAsset("lambda"),
       handler: "main",
       runtime: lambda.Runtime.GO_1_X,
+      environment: {
+        // Rename to user table
+        TABLE_NAME: table.tableName,
+      },
     });
+
+    // Grant Lambda function
+    table.grantReadWriteData(myFunction);
 
     // Define the API Gateway
     const api = new apigw.RestApi(this, "Endpoint");
